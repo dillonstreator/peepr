bb92e21105b432f7c5e4b3416c683e7a855d6d50
dillonstreator <dillonstreator@gmail.com>
2020-06-28 11:14:23 -0500

    test2

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React, { Suspense } from "react";
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import store from './store';
 
 import styles from "./app.module.scss";
-
-export const history = createBrowserHistory({
-	basename: process.env.PUBLIC_URL,
-});
 
 const Chats = React.lazy(() => import('./pages/Chats/Chats'));
 const ChatsLazy = (props) => (<Suspense fallback={<div>Loading...</div>}><Chats {...props} /></Suspense>)
@@ -21,7 +16,7 @@ function App() {
 	return (
 		<div className={styles.App}>
 			<Provider store={store}>
-				<Router history={history}>
+				<Router basename={process.env.PUBLIC_URL}>
 					<Switch>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/chats" component={ChatsLazy} />
